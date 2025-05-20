b3da5e9d854a854723a0dde8d66d9739f7fa1319
macricardo <300877933@pcpuma.acatlan.unam.mx>
2025-05-19 21:34:15 -0600

    Beautifying backgrounds.

--- src/pages/PatientProfile.jsx
+++ src/pages/PatientProfile.jsx
@@ -1,6 +1,31 @@
+import styled, { keyframes } from "styled-components";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+
+const gradientMove = keyframes`
+  0% {
+    background-position: 50% 50%;
+  }
+  50% {
+    background-position: 55% 45%;
+  }
+  100% {
+    background-position: 50% 50%;
+  }
+`;
+
+const Background = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  position: fixed;
+  inset: 0;
+  z-index: -1;
+  background: radial-gradient(circle at 50% 50%,rgb(255, 255, 255) 0%,rgb(132, 133, 216) 40%,rgb(162, 169, 201) 70%, #fff 100%);
+  background-size: 200% 200%;
+  animation: ${gradientMove} 8s ease-in-out infinite;
+`;
+
 
 const PatientProfile = () => {
   const { id } = useParams();
@@ -42,6 +67,7 @@ const PatientProfile = () => {
 
   return (
     <div>
+      <Background />
       <Navbar />
       <div className="p-6 max-w-3xl mx-auto">
         <h1 className="text-2xl font-bold mb-4">Perfil de {paciente.nombre}</h1>
